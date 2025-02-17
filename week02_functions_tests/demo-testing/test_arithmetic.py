1765a8bd69245b9ec096b15971e9bd4ffad0552d
aruiz <s243329@dtu.dk>
2025-02-17 18:07:05 +0100

    Tutorial exercises

--- week02_functions_tests/demo-testing/test_arithmetic.py
+++ week02_functions_tests/demo-testing/test_arithmetic.py
@@ -1,5 +1,6 @@
 """Check some of the functions in arithmetic.
 """
+import numpy as np
 from arithmetic import square
 
 
@@ -14,8 +15,24 @@ def test_square_integer():
     # then
     assert y == y_theo
 
+def test_square_float():
+    # Given
+    x = 3.4
+    y_theo = 11.56
+    # When
+    y = square(x)
+    # Then
+    assert np.isclose(y,y_theo)
+
 
 # code to execute only if Python is executed directly on this module,
 # NOT on import
 if __name__ == '__main__':
     test_square_integer()
+    test_square_float()
+
+
+
+
+
+
